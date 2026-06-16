27220a8c9562f3a65c339825baa062f568daf0c4
parzival-xr <136064755+parzival-xr@users.noreply.github.com>
2026-06-16 11:25:58 +0800

    注释签名内容

--- simple_live_app/android/app/build.gradle.kts
+++ simple_live_app/android/app/build.gradle.kts
@@ -41,12 +41,12 @@ android {
 
     signingConfigs {
         create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
-            storePassword = keystoreProperties["storePassword"] as String
-            isV1SigningEnabled = true
-            isV2SigningEnabled = true
+            // keyAlias = keystoreProperties["keyAlias"] as String
+            // keyPassword = keystoreProperties["keyPassword"] as String
+            // storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            // storePassword = keystoreProperties["storePassword"] as String
+            // isV1SigningEnabled = true
+            // isV2SigningEnabled = true
         }
     }
 
